056be8d22eff6aca4316aafd597a857a604359ff
Matthew Ferderber <matthewferderber@gmail.com>
2017-04-13 02:35:57 -0400

    Added API_URL env variable to config

--- vbuild.config.js
+++ vbuild.config.js
@@ -8,10 +8,12 @@ module.exports = options => ({
     // add more postcss plugins here
     // by default we have autoprefixer pre added
   ],
+  env: {
+    API_URL: process.NODE_ENV === 'production' ? '/api' : '//localhost:3000/api'
+  },
   webpack (config) {
     config.resolve.modules.push(path.resolve('src'));
     config.resolve.modules.push(path.resolve('config.js'));
-
     // inject offline-plugin in production build
     if (!options.dev) {
       config.plugins.push(new OfflinePlugin({
